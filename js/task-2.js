79363151d5688e72ea534344b3294cb979e33721
Nataliia Strokan <nataliia.strokan@gmail.com>
2026-03-28 17:00:52 +0200

    prettier for task-2 and task-3

--- js/task-2.js
+++ js/task-2.js
@@ -9,20 +9,20 @@ function makeArray(firstArray, secondArray, maxLength) {
 }
 
 // Перевірка (не видаляти)
-console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); 
+console.log(makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3));
 // ["Mango", "Poly", "Ajax"]
 
-console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); 
+console.log(makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea'], 4));
 // ["Mango", "Poly", "Houston", "Ajax"]
 
-console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); 
+console.log(makeArray(['Mango'], ['Ajax', 'Chelsea', 'Poly', 'Houston'], 3));
 // ["Mango", "Ajax", "Chelsea"]
 
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); 
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 2));
 // ["Earth", "Jupiter"]
 
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); 
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 4));
 // ["Earth", "Jupiter", "Neptune", "Uranus"]
 
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); 
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus', 'Venus'], 0));
 // []
